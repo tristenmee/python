31aef54eba7aceae98ac0e8e1762fb7c83ff7067
Tristen Mee <Tristen0717@gmail.com>
2013-12-16 15:12:08 -0500

    Update mathquiz.py

--- mathquiz.py
+++ mathquiz.py
@@ -1,32 +1,54 @@
 # FILE: Mathquiz.py
-
+ 
 from random import randint 
-from goto import goto, comefrom, label
-
+ 
 correct = 0
-questions = 0
-
-duration = input("How many questions would you like to answer?")
-
+ 
+duration = input("How many questions would you like to answer? ")
+ 
 level = raw_input("What level, (E)asy, (M)edium, or (H)ard? ")
-
-label .start
+ 
 if level == 'E':
-	for i in range(10):
-   			n1 = randint(1, 10)
-   			n2 = randint(1, 10)
-   			product = n1 * n2
-   			answer = input("What's" + str(n1) + " times " + str(n2) + "? ")
-   	if answer == product:
-   		print "That's right -- well done."
-   		correct = correct + 1
-   		questions = questions + 1
+  for i in range(duration):
+    n1 = randint(1, 10)
+    n2 = randint(1, 10)
+    product = n1 * n2
+    answer = input("What's" + str(n1) + " times " + str(n2) + "? ")
+    if answer == product:
+      print "That's right -- well done."
+      correct = correct + 1
+    else:
+       print "No, I'm afraid the answer is " + str(product) + "."
 
-   		if duration >= questions:
-   	    		goto .start
-	else:
-		print "No, I'm afraid the answer is" + str(product) + "."
-		questions = questions + 1
-		
-		if duration >= questions:
-   	    		goto .start
+
+
+if level == 'E':
+  for i in range(duration):
+    n1 = randint(1, 25)
+    n2 = randint(1, 25)
+    product = n1 * n2
+    answer = input("What's" + str(n1) + " times " + str(n2) + "? ")
+    if answer == product:
+      print "That's right -- well done."
+      correct = correct + 1
+    else:
+       print "No, I'm afraid the answer is " + str(product) + "."
+
+
+
+if level == 'E':
+  for i in range(duration):
+    n1 = randint(1, 100)
+    n2 = randint(1, 100)
+    product = n1 * n2
+    answer = input("What's" + str(n1) + " times " + str(n2) + "? ")
+    if answer == product:
+      print "That's right -- well done."
+      correct = correct + 1
+    else:
+       print "No, I'm afraid the answer is " + str(product) + "."
+
+
+
+print "I asked you " + str(duration) + " questions.  You got " + str(correct) + " of them right.\n"
+print "Well done!"
