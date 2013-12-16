bff87d01ade32adf88d8f1e87559bb080c84f153
Tristen Mee <Tristen0717@gmail.com>
2013-12-16 14:49:39 -0500

    Update mathquiz.py

--- mathquiz.py
+++ mathquiz.py
@@ -18,17 +18,15 @@ if level == 'E':
    			product = n1 * n2
    			answer = input("What's" + str(n1) + " times " + str(n2) + "? ")
    	if answer == product:
-   		    print "That's right -- well done."
-   		    correct = correct + 1
-   		    questions = questions + 1
+   		print "That's right -- well done."
+   		correct = correct + 1
+   		questions = questions + 1
 
-   	elif duration >= questions:
+   		if duration >= questions:
    	    		goto .start
-
-
 	else:
-   	    print "No, I'm afraid the answer is" + str(product) + "."
-   	    questions = questions + 1
-
-   	    if duration >= questions:
+		print "No, I'm afraid the answer is" + str(product) + "."
+		questions = questions + 1
+		
+		if duration >= questions:
    	    		goto .start
