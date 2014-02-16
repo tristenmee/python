10ac4d65e19ef4f2f889da7de2c5c5e0dc07a708
Tristen Mee <tristen0717@gmail.com>
2014-02-16 15:50:09 -0500

    Update non-repeatpass.py

--- non-repeatpass.py
+++ non-repeatpass.py
@@ -15,7 +15,7 @@ password = "89bd"
 attempts = 0
 
 
-#Opens the file that will hold the attempted passwords
+#List that contains possible digits
 possibleDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 
 
